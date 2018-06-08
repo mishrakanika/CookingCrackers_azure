6422b0bf73db86cbcc84e9e7f9d238679ab7a4ad
Rutu Bhatt <rutubhatt80@gmail.com>
2018-06-07 20:13:58 -0700

    test working fine for get and post

--- Test/Test/test.js
+++ Test/Test/test.js
@@ -18,7 +18,7 @@ chai.use(chaiHttp);
 	// 			  .get("/app/recipe/")
 	  before(function (done) {
 		chai.request("http://cookingcracker.azurewebsites.net")
-		.get("/#/allrecipes/")
+		.get("/#/getRecipe/1/")
 				  .end(function (err, res) {
 					  requestResult = res.body;
 					  response = res;
@@ -30,13 +30,13 @@ chai.use(chaiHttp);
 	  
 	  
 	//   it('The first entry in the recipe array has known properties', function(){
-	// 		  expect(requestResult[0]).to.include.keys('_id');
-	// 		  expect(requestResult[0]).to.have.property('rrecipeId');
-	// 		  expect(requestResult[0]).to.have.property('rname');
-	// 		  expect(requestResult[0]).to.have.property('rmethod');
-	// 		  expect(requestResult[0]).to.have.property('rdescription');
-	// 		  expect(requestResult[0]).to.have.property('rduration');
-	// 		  expect(requestResult[0]).to.have.property('ringredients');
+	// 		  //expect(requestResult).to.include.keys('_id');
+	// 		  expect(response.body[0]).to.have.property('rrecipeId');
+	// 		//   expect(requestResult[0]).to.have.property('rname');
+	// 		//   expect(requestResult[0]).to.have.property('rmethod');
+	// 		//   expect(requestResult[0]).to.have.property('rdescription');
+	// 		//   expect(requestResult[0]).to.have.property('rduration');
+	// 		//   expect(requestResult[0]).to.have.property('ringredients');
 	// 		  expect(response.body).to.not.be.a.string;
 	// 	  });
 	  
@@ -74,3 +74,48 @@ chai.use(chaiHttp);
 		 
 	  
 	  });	
+
+	  describe('Test post recipes', function () {
+		this.timeout(150000);
+		  var requestResult;
+		  var response;
+			   
+	  before(function (done) {
+	  let recipe={
+			rname: "Test Post Recipe",
+			rdescription:"Upma, is a dish from the Indian subcontinent, cooked as a thick porridge from dry roasted semolina or coarse rice flour.",
+			rmethod: "1 Place semolina in cast iron pan and cook over medium heat until toasty but not browned, about 2 minutes. Transfer to a bowl.2.Return pan to medium heat and heat ghee or oil until shimmering. Add the mustard seeds and split black gram, stir until they splutter, about 30 seconds. Add the onion, fry until slightly soft, about a minute (take care not to brown). Add the slit chillis, curry leaves and stir until fragrant, about a minute longer. Add the carrots, peas, red chilli powder, and sugar, and stir until fragrant and the mixture is coated with the ghee/oil, about 1 minute.3.Add toasted semolina and stir vigorously until the mixture resembles wet sand—about a minute. Add the salt and gently pour in the water. The semolina will bubble and spurt as it absorbs the water. Reduce heat to low. Allow this mixture to cook for about 2 minutes, stirring constantly. The Upma absorbs water very quickly and tends to dry out too, so turn off the heat when the mixture is slightly runny. Serve immediately.",
+			rcuisinetype: "Indian",
+			rmealpreference: "Vegetarian",
+			rmealtype: "Breakfast",
+			rduration: 15,
+			ringredients: "1 cup semolina flour, 1 1/2 tablespoons ghee or vegetable oil, 1 teaspoon whole black mustard seeds, 1 teaspoon split black gram (Urad dal), 1 small onion chopped (about 1/2 cup), 2 small green chills split lengthwise, 10 curry leaves, 1/4 cup diced boiled carrots, 1/4 cup boiled green peas, 1 teaspoon red chilli powder, 1 teaspoon sugar, Kosher salt, 2 1/4 cups water",
+			
+		  }
+		  chai.request("http://cookingcracker.azurewebsites.net")
+				  .post('/app/recipe/')
+				  .send(recipe)
+				  .end(function(err, res)  {
+					  requestResult = res.body;
+					  response = res;
+					  expect(res).to.have.status(200);
+					  done();
+				  });
+				  });
+	  
+	  it('It should have the success status response ', function(){  
+			  
+			  expect(response.body).to.have.property('message');
+	  });
+	  
+	  it('It should have the known property in response body', function(){  
+			  
+			  expect(response.body).to.have.property('message').to.be.a('string');
+	  });
+	  
+	  it('It should have the expected value in response body', function(){  
+			  
+			  expect(response.body).to.have.property('message').eql("Recipe created!");
+	  });
+	  
+			  });
