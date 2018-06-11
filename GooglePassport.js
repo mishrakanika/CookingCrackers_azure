676a46f5fcaaacbc6304204516385621222aaf40
Rutu Bhatt <rutubhatt80@gmail.com>
2018-06-11 13:11:03 -0700

    compiled google ts file

--- GooglePassport.js
+++ GooglePassport.js
@@ -6,6 +6,7 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 // Creates a Passport configuration for Google
 var GooglePassport = /** @class */ (function () {
     function GooglePassport() {
+        var _this = this;
         this.clientId = googleOauth2_1["default"].id;
         this.secretId = googleOauth2_1["default"].secret;
         passport.use(new GoogleStrategy({
@@ -13,18 +14,15 @@ var GooglePassport = /** @class */ (function () {
             clientSecret: this.secretId,
             callbackURL: "http://cookingcracker.azurewebsites.net/auth/google/callback",
             profileFields: ['id', 'displayName', 'emails']
-        }
-        //,
-        // (accessToken, refreshToken, profile, done) => {
-        //     process.nextTick( () => {
-        //         console.log('validating facebook profile:' + JSON.stringify(profile));
-        //         this.userId = profile.id;
-        //         this.displayName = profile.displayName;
-        //         this.email = profile.emails[0].value;
-        //         return done(null, profile);
-        //     });
-        // }
-        ));
+        }, function (accessToken, refreshToken, profile, done) {
+            process.nextTick(function () {
+                console.log('validating profile:' + JSON.stringify(profile));
+                _this.userId = profile.id;
+                _this.displayName = profile.displayName;
+                _this.email = profile.emails[0].value;
+                return done(null, profile);
+            });
+        }));
         passport.serializeUser(function (user, done) {
             done(null, user);
         });
